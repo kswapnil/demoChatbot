9a8aae18f6f0b7d6ca5ce71b085405b0f4e24a63
Swapnil Jagannath Khandizod <Swapnil.Khandizod@ATMECS.CORP>
2017-09-11 18:39:25 +0530

    second commit

--- src/main/java/com/swap/demo/controller/HelloController.java
+++ src/main/java/com/swap/demo/controller/HelloController.java
@@ -16,19 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 	@RequestMapping(value="/getDiveSites/{country}",method=RequestMethod.GET,headers="Accept=application/json")
-	public @ResponseBody List<String> getAllDiveSites(@PathVariable("country")String country){
+	public @ResponseBody String getAllDiveSites(@PathVariable("country")String country){
 		
-		Map<String, List<String>> mapCountries = new HashMap<>();
+		/*Map<String, List<String>> mapCountries = new HashMap<>();
 		List<String> listIndia = Arrays.asList("Planet Scuba India","Scuba Evolution India","Dive Goa","Temple Adventures");	
 		mapCountries.put("India", listIndia);
 		List<String> listUsa = Arrays.asList("PADI Americas","Divers Direct","Scuba Works","US Scuba Center");	
-		mapCountries.put("usa", listUsa);
+		mapCountries.put("USA", listUsa);
 		List<String> listSrilanka = Arrays.asList("Poseidon Diving Centre Mirissa","Pearl Divers PADI","Island Scuba","Submarine Diving");	
-		mapCountries.put("srilanka", listSrilanka);
+		mapCountries.put("CUBA", listSrilanka);
 		
 		if(mapCountries.containsKey(country)){
 			return mapCountries.get(country);
-		}
-		return null;
+		}*/
+		return "Hello World";
 	}
 }
