d319e62ce31b5ac3521c74a4840b8c4f5a994271
Swapnil Jagannath Khandizod <Swapnil.Khandizod@ATMECS.CORP>
2017-09-12 12:35:11 +0530

    update final

--- src/main/java/com/swap/demo/controller/HelloController.java
+++ src/main/java/com/swap/demo/controller/HelloController.java
@@ -1,15 +1,14 @@
 package com.swap.demo.controller;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 	@RequestMapping(value="/getDiveSites",method=RequestMethod.POST)
-	public  @ResponseBody WebhookResponse getAllDiveSites(@RequestBody String country) throws JSONException{
+	public  @ResponseBody WebhookResponse getAllDiveSites(@RequestBody String country){
 
 		Map<String, List<String>> mapCountries = new HashMap<>();
 		List<String> listIndia = Arrays.asList("Planet Scuba India","Scuba Evolution India","Dive Goa","Temple Adventures");	
@@ -29,11 +28,16 @@ public class HelloController {
 		mapCountries.put("USA", listUsa);
 		List<String> listSrilanka = Arrays.asList("Poseidon Diving Centre Mirissa","Pearl Divers PADI","Island Scuba","Submarine Diving");	
 		mapCountries.put("CUBA", listSrilanka);
-		
+		String key=null;
+		try{
 		JSONObject jsonObj = new JSONObject(country);
 		JSONObject result=(JSONObject)jsonObj.get("result");
-		
-		String key=result.getString("resolvedQuery");
+		JSONArray contexts=(JSONArray)result.get("contexts");
+		JSONObject FirstJsonObject=(JSONObject)contexts.getJSONObject(0);
+		JSONObject parameters=(JSONObject)FirstJsonObject.get("parameters");
+		key=parameters.getString("geo-country.original");
+		}
+		catch(JSONException e){return new WebhookResponse("JSON parsing Exception","JSON parsing Exception");}
 		
 		if(mapCountries.containsKey(key)){
 			return new WebhookResponse(mapCountries.get(key).toString(), mapCountries.get(key).toString());
